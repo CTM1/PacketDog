419599f5ca281959b1bb79fe302da863426e6081
krkn <claudio.t1337@gmail.com>
2021-09-17 16:53:12 +0200

    Removed old sniffer script, created Sniffer class

--- OOPsniffer.py
+++ OOPsniffer.py
@@ -7,37 +7,33 @@ from protocols.transport import icmp
 from protocols.transport import tcp
 from protocols.transport import udp
 
-
 IPV4HEADER = 8
-
 tcpProto  = (tcp, "TCP", 6),
 udpProto  = (udp, "UDP", 17),
 icmpProto = (icmp, "ICMP", 1)
-
 transportProtocols = [tcpProto, udpProto, icmpProto]
 
-def main():
-    conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-    while (True):
-        rawData, addr = conn.recvfrom(9000)
+class Sniffer():
+    def __init__(self, params):
+        self.params = params
 
-        ethPacket = ethFrame.ethFrame(rawData)
-        ethPacket.unpack()
-        ethPacket.printInfo()
+    def sniff(self.params):
+        conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+        while (True):
+            rawData, addr = conn.recvfrom(9000)
+            ethPacket = ethFrame.ethFrame(rawData)
+            ethPacket.unpack()
+            ethPacket.printInfo()
 
-        if (ethPacket.protocol == IPV4HEADER):
-            IPv4 = ipv4.IPv4(ethPacket.payload)
-            IPv4.unpack()
-            IPv4.printInfo()
+            if (ethPacket.protocol == IPV4HEADER):
+                IPv4 = ipv4.IPv4(ethPacket.payload)
+                IPv4.unpack()
+                IPv4.printInfo()
 
-            for proto in transportProtocols:
-                if (IPv4.protocol == proto[0][2]):
-                    _class_ = getattr(proto[0][0], proto[0][1])
-                    instance = _class_(IPv4.ipv4Data)
-                    instance.unpack()
-                    instance.printInfo()
-                    break
-
-
-
-main()
+                for proto in transportProtocols:
+                    if (IPv4.protocol == proto[0][2]):
+                        _class_ = getattr(proto[0][0], proto[0][1])
+                        instance = _class_(IPv4.ipv4Data)
+                        instance.unpack()
+                        instance.printInfo()
+                        break
